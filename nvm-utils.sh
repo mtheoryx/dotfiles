e0e32e63dd805788de248102cca2f342e752c8c5
David Poindexter <david.poindexter@blackboard.com>
2017-11-17 13:01:35 -0500

    Experimental: New node versions, re-install some npm packages globally

--- nvm-utils.sh
+++ nvm-utils.sh
@@ -4,3 +4,11 @@
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
+
+# Setup stuff we like in new nvm/node/npm environment
+
+migratenode () {
+  echo 'Starting node package migration...'
+  (cd ~; npm install yarn; npm install create-react-app)
+  echo 'Done migrating desired node packages.'
+}
